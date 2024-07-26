6c4e5d1b9ddf7e7c5868c15664f327e64b03c4cd
Giwa MUhammad <mgiwa78@gmail.com>
2024-07-26 09:20:24 +0100

    A

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,8 @@
-export const API_BASE_URL = "http://localhost:5000/support-api";
-export const ASSETS_URL = "http://localhost:5000/";
+export const API_BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5000/support-api"
+    : `${process.env.VITE_APP_API_URL}api/`;
+export const ASSETS_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5000/"
+    : `${process.env.VITE_APP_API_URL}`;
